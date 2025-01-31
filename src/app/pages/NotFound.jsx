cc259b8338fbf58c5cb930a15c5011a3b3ea90e4
Daniel D Goncalves <107401263+GoncalvesDDaniel@users.noreply.github.com>
2025-01-31 10:52:17 +0100

    Add 404 page

--- src/app/pages/NotFound.jsx
+++ src/app/pages/NotFound.jsx
@@ -1,5 +1,18 @@
+import React from "react";
+import { NavLink } from "react-router";
+
 function NotFound() {
-    return <h1>NotFound</h1>;
+    return (
+        <>
+            <p className="notFound">404</p>
+            <p className="notFound-subtitle">
+                Oups! La page que vous demandez n'existe pas.
+            </p>
+            <NavLink to="/" className="notFound-link">
+                Retourner sur la page d'acceuil
+            </NavLink>
+        </>
+    );
 }
 
 export default NotFound;
--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { NavLink } from "react-router";
+import Logo from "../../assets/images/Logo.svg";
 
 function Header() {
     return (
         <header className="header flex justify-between items-center ">
-            <img
-                className="header-logo"
-                src="./src/assets/images/Logo.svg"
-                alt="Logo de Kasa"
-            ></img>
+            <img className="header-logo" src={Logo} alt="Logo de Kasa"></img>
             <nav className="header-nav flex">
                 <NavLink to={"/"}>Accueil</NavLink>
                 <NavLink to={"/about"}>À propos</NavLink>
